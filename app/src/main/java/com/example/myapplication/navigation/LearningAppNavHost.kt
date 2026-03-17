15c09cac9e60318a2e3c36a31d732b28a8cc656e
dmitrylebedev <dimitrylebedev@yandex.ru>
2026-03-17 22:03:22 +0300

    Настроить экран списка как стартовый через Compose Navigation

---
+++ app/src/main/java/com/example/myapplication/navigation/LearningAppNavHost.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.myapplication.feature.catalog.AppCatalogRoute
+
+object LearningAppDestination {
+    const val CATALOG = "catalog"
+}
+
+@Composable
+fun LearningAppNavHost(
+    navController: androidx.navigation.NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier,
+) {
+    NavHost(
+        navController = navController,
+        startDestination = LearningAppDestination.CATALOG,
+        modifier = modifier,
+    ) {
+        composable(LearningAppDestination.CATALOG) {
+            AppCatalogRoute()
+        }
+    }
+}
